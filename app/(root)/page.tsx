838c8e99975cbf5a484dbf15eff3407e8f6f5655
JustRaff <rafflymaulana5@gmail.com>
2023-12-22 14:20:38 +0700

    onboarding  error

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   const result = await fetchPosts(1, 30)
   const user = await currentUser()
 
-  if (!user) return null;
+  if (!user) redirect("/sign-in");
 
   const userInfo = await fetchUser(user.id);
 
